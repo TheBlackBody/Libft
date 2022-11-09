f7f28013df6a74d1289cc8a5cfe94526d1316324
TheBlackBody <66089114+TheBlackBody@users.noreply.github.com>
2022-11-09 17:51:04 +0100

    a

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,11 +6,38 @@
 /*   By: sfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 15:04:42 by sfernand          #+#    #+#             */
-/*   Updated: 2022/11/09 15:37:25 by sfernand         ###   ########.fr       */
+/*   Updated: 2022/11/09 17:09:02 by sfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <string.h>
+
+int	ft_lenght1(const char *src, size_t	n)
+{
+	size_t	i;
+	int		*a;
+	int		b;
+
+	i = 0;
+	a = (int *)src;
+	b = 0;
+	while (i < n)
+	{
+		b = b + a[i];
+		i++;
+	}	
+	return (b);
+}
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	
+	size_t	i;
+
+	i = 0;
+	if (ft_lenght1(s1, n) == ft_lenght1(s2, n))
+		return (0);
+	else if (ft_lenght1(s1, n) < ft_lenght1(s2, n))
+		return (-1);
+	else if (ft_lenght1(s1, n) < ft_lenght1(s2, n))
+		return (1);
+	return (0);
 }
