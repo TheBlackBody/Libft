46fdf2be519766ec5c2ff81c9fccfd076ad8c7a6
TheBlackBody <66089114+TheBlackBody@users.noreply.github.com>
2022-11-10 17:06:23 +0100

    a

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,31 @@
 /*   By: sfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 15:27:26 by sfernand          #+#    #+#             */
-/*   Updated: 2022/11/10 15:28:08 by sfernand         ###   ########.fr       */
+/*   Updated: 2022/11/10 17:03:57 by sfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *str)
 {
+	int	neg;
+	int	i;
+	int	num;
 
+	i = 0;
+	neg = 1;
+	num = 0;
+	while (str[i] == ' ' || str[i] == 0)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		num = num * 10 + (str[i] - 48);
+		i++;
+	}
+	return (num * neg);
 }
